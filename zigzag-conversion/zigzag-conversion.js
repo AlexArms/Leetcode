fb00148e577db6a4c9ae5502d4df015a0140db95
Alex Armstrong <alexaarms96@gmail.com>
2021-08-03 12:16:50 -0400

    Time: 116 ms (42.65%), Space: 45.6 MB (29.51%) - LeetHub

--- zigzag-conversion/zigzag-conversion.js
+++ zigzag-conversion/zigzag-conversion.js
@@ -3,6 +3,25 @@
  * @param {number} numRows
  * @return {string}
  */
+// var convert = function(s, numRows) {
+//   let transformed = [];
+//   let rows = numRows;
+//   while (rows) {
+//     transformed.push([]);
+//     rows--;
+//   }
+//   let rowToAdd = 0;
+//   let direction = 'increase';
+//   for (let i = 0; i < s.length; i++) {
+//     transformed[rowToAdd].push(s[i]);
+//     if (direction === 'increase' && rowToAdd === numRows - 1) direction = 'decrease';
+//     if (direction === 'increase' && rowToAdd < numRows - 1) rowToAdd++;
+//     if (direction === 'decrease' && rowToAdd > 0) rowToAdd--;
+//     if (direction === 'decrease' && rowToAdd === 0) direction = 'increase';
+//   }
+//   return transformed.flat().join('');
+// };
+
 var convert = function(s, numRows) {
   let transformed = [];
   let rows = numRows;
@@ -11,14 +30,13 @@ var convert = function(s, numRows) {
     rows--;
   }
   let rowToAdd = 0;
-  let direction = 'increase';
+  let increase = true;
   for (let i = 0; i < s.length; i++) {
     transformed[rowToAdd].push(s[i]);
-    if (direction === 'increase' && rowToAdd === numRows - 1) direction = 'decrease';
-    if (direction === 'increase' && rowToAdd < numRows - 1) rowToAdd++;
-    if (direction === 'decrease' && rowToAdd > 0) rowToAdd--;
-    if (direction === 'decrease' && rowToAdd === 0) direction = 'increase';
-
+    if (increase && rowToAdd === numRows - 1) increase = !increase;
+    if (increase && rowToAdd < numRows - 1) rowToAdd++;
+    if (!increase && rowToAdd > 0) rowToAdd--;
+    if (!increase && rowToAdd === 0) increase = !increase;
   }
   return transformed.flat().join('');
 };
