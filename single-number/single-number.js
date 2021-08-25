ea6dc8dcb5803cb65b8df742ccc56a66c11ac72f
Alex Armstrong <alexaarms96@gmail.com>
2021-08-25 09:48:33 -0400

    Time: 76 ms (90.67%), Space: 41.1 MB (97.14%) - LeetHub

--- single-number/single-number.js
+++ single-number/single-number.js
@@ -3,13 +3,7 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-  const seenVals = {};
-  for (let i = 0; i < nums.length; i++) {
-    if (seenVals[nums[i]] !== undefined) {
-      delete seenVals[nums[i]];
-    } else {
-      seenVals[nums[i]] = nums[i];
-    }
-  }
-  return Object.keys(seenVals)[0];
+  let outlier = 0;
+  nums.forEach(num => outlier ^= num);
+  return outlier;
 };
