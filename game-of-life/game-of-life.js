536d9ed7f1e3357378a5cb2331841c49861537b9
Alex Armstrong <alexaarms96@gmail.com>
2021-12-29 19:04:59 -0500

    Time: 72 ms (88.22%), Space: 40.8 MB (16.56%) - LeetHub

--- game-of-life/game-of-life.js
+++ game-of-life/game-of-life.js
@@ -3,12 +3,11 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var gameOfLife = function(board) {
-    const boardCopy = board.map(row => row.map(cell => cell));
+    const boardCopy = copyMatrix(board);
     board.forEach((row, i) => {
         row.forEach((cell, j) => {
-            const surroundingSum = getSurroundingSum(i, j, boardCopy);
-            if (surroundingSum < 2 || surroundingSum > 3) board[i][j] = 0;
-            if (surroundingSum === 3) board[i][j] = 1;
+            // const surroundingSum = ;
+            setCellVal(i, j, getSurroundingSum(i, j, boardCopy), board);
         });
     });
 };
@@ -17,9 +16,20 @@ const getSurroundingSum = (row, col, board) => {
     let sum = 0;
     const directions = [[0, 1], [0, -1], [-1, 0], [1, 0], [1, 1], [-1, -1], [-1, 1], [1, -1]];
     directions.forEach(([dRow, dCol]) => {
-        if (board[row + dRow] && board[row + dRow][col + dCol]) sum++; 
+        if (board[row + dRow] && board[row + dRow][col + dCol]) {
+            sum++; 
+        }
     });
     return sum;
+}
+
+const setCellVal = (row, col, sum, board) => {
+    if (sum < 2 || sum > 3) board[row][col] = 0;
+    if (sum === 3) board[row][col] = 1;
+}
+
+const copyMatrix = (matrix) => {
+    return matrix.map(row => row.map(cell => cell));
 }
 
 // convert seen vals to strings
